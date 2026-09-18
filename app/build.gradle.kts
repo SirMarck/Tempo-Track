e98d8cae288278491cfa9ee5b813f32815fdbcfb
Marcos Brandler <marcosbrandler@users.noreply.github.com>
2026-09-18 00:48:33 -0300

    fix(build): remove obsolete moshi-ksp-codegen to make Room run smoothly on KSP2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -120,5 +120,4 @@ dependencies {
   debugImplementation(libs.androidx.compose.ui.test.manifest)
   debugImplementation(libs.androidx.compose.ui.tooling)
   "ksp"(libs.androidx.room.compiler)
-  "ksp"(libs.moshi.kotlin.codegen)
 }
